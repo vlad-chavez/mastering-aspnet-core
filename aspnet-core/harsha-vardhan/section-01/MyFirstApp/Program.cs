c956c238eb664648ea6a707c883625ebf43e8b00
Vladimir Chavez <vldim.chz@gmail.com>
2025-10-04 12:58:17 -0500

    Add response content type and status code handling in Program.cs

--- aspnet-core/harsha-vardhan/section-01/MyFirstApp/Program.cs
+++ aspnet-core/harsha-vardhan/section-01/MyFirstApp/Program.cs
@@ -2,8 +2,25 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
+app.Run(async (HttpContext context) =>
+{
 
 
-app.MapGet("/", () => "Hello World!");
+    /*
+    Set the response content type and status code
+    context.Response.Headers.ContentType = "text/plain";
+    context.Response.Headers["Content-Type"] = "text/plain";
+    context.Response.ContentType = "text/plain";
+    */
+    context.Response.ContentType = "text/plain";
+    /*
+    Set the response status code
+    context.Response.StatusCode = 200;
+    context.Response.StatusCode = StatusCodes.Status200OK;
+    context.Response.Headers["Status-Message"] = "OK";
+    */
+    context.Response.StatusCode = StatusCodes.Status200OK;
+    await context.Response.WriteAsync("Hello, World!");
+});
 
 app.Run();
